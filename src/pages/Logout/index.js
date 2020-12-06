b9dca30609d351966629ce9438f110fdcb8e3c43
Marcus Lima <marcus_fonseca@ymail.com>
2020-12-06 17:50:14 -0300

    Logout page

--- src/pages/Logout/index.js
+++ src/pages/Logout/index.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuth } from "../../contexts/auth";
+import { Redirect } from "react-router-dom";
 
 const Logout = props => {
-  return (
-    <p>Logout</p>
-  )
+  const { setAuthTokens, setTokenIsValid } = useAuth();
+
+  useEffect(() => {
+    setAuthTokens();
+    setTokenIsValid(false);
+    // eslint-disable-next-line
+  }, []);
+
+  return <Redirect to="/login" />;
 }
 
 export default Logout;
